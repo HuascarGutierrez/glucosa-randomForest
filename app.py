b2ee8b465f63297cd0fddea346b99aba89994f41
Huascar Gutierrez <huascargutierrezcastro9951591@gmail.com>
2025-01-30 17:12:21 -0400

    changes on app

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ async def predecir(Glucose: float, BMI: float):
     scaler = StandardScaler()
     nuevo = np.array([[Glucose, BMI]])
     nuevo_scaled = nuevo
-    # nuevo_scaled = scaler.fit_transform(nuevo)
+    nuevo_scaled = scaler.fit_transform(nuevo)
     prediccion = modelo.predict_proba(nuevo_scaled)
     return {'prediccion': prediccion[0][1]}
